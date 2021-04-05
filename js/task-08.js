2e4513d8591a02fe8d5a5a4107571081cbc3bf0b
virysik <68701496+virysik@users.noreply.github.com>
2021-04-05 19:48:44 +0200

    started with task 8

--- js/task-08.js
+++ js/task-08.js
@@ -4,16 +4,26 @@ const renderBtn = document.querySelector('[data-action="render"]');
 const destroyBtn = document.querySelector('[data-action="destroy"]');
 const boxesEl = document.querySelector('#boxes');
 
-const max = 250;
-const min = 0;
-let numR = Math.round(Math.random() * (max - min) + min);
-let numG = Math.round(Math.random() * (max - min) + min);
-let numB = Math.round(Math.random() * (max - min) + min);
+const createBoxes = () => {
+    const max = 250;
+    const min = 0;
+    let numR = Math.round(Math.random() * (max - min) + min);
+    let numG = Math.round(Math.random() * (max - min) + min);
+    let numB = Math.round(Math.random() * (max - min) + min);
 
-const boxEl = document.createElement('div');
-boxEl.id = "box";
-boxEl.style.backgroundColor = `rgb(${numR},${numG},${numB})`;
-boxEl.style.width = '30px';
-boxEl.style.height = '30px';
+    const boxEl = document.createElement('div');
+    boxEl.id = "box";
+    boxEl.style.backgroundColor = `rgb(${numR},${numG},${numB})`;
+    boxEl.style.width = '30px';
+    boxEl.style.height = '30px';
 
-boxesEl.appendChild(boxEl);
+    boxesEl.appendChild(boxEl);
+}
+
+    const onClean = () => {
+        boxesEl.innerHTML = ' ';
+    };
+
+
+renderBtn.addEventListener('click', createBoxes);
+destroyBtn.addEventListener('click', onClean);
