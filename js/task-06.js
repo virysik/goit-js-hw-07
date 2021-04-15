959b44abf454a27ba8aaa565b588d1d021fdd718
virysik <68701496+virysik@users.noreply.github.com>
2021-04-15 17:30:51 +0200

    made check changes in task 6

--- js/task-06.js
+++ js/task-06.js
@@ -5,10 +5,17 @@ const onBlur = () => {
     const elLength = Number(inputEl.dataset.length);
     const symbolLength = inputEl.value.length;
     
-    if ( symbolLength!== elLength) {
+    if (symbolLength !== elLength) {
+        if (inputEl.classList.contains('valid')) {
+            inputEl.classList.remove("valid");
+        }
         inputEl.classList.add("invalid");
-    }
+    } else {
+        if (inputEl.classList.contains('invalid')) {
+            inputEl.classList.remove("invalid");
+        }
     inputEl.classList.add("valid");
+}
 };
 
 inputEl.addEventListener('blur', onBlur);
